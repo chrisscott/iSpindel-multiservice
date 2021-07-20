3c4f7736d4b979e7b902620e351dfd0622074f3f
Chris Scott <chris@iamzed.com>
2021-07-20 19:58:41 -0400

    HA

--- src/services/homeassistant.ts
+++ src/services/homeassistant.ts
@@ -12,43 +12,57 @@ export default async (request: FastifyRequest): Promise<void> => {
   const services = config.services.filter((service) => service.type === 'homeassistant');
   const data: IspindelData = request.body as IspindelData;
 
-  const {
-    temperature,
-    battery,
-    gravity,
-    interval,
-    RSSI,
-  } = data;
+  const postData = async (
+    url: string | undefined,
+    deviceLabel: string,
+    stateName: string,
+    state: unknown,
+    unitOfMeasurement: string,
+    token: string | undefined,
+    axiosConfig: AxiosRequestConfig,
+  ) => {
+    const payload = {
+      state,
+      attributes: {
+        unit_of_measurement: unitOfMeasurement,
+        friendly_name: `${deviceLabel} ${stateName}`,
+      },
+    };
 
-  services.forEach(async (service) => {
-    const { deviceLabel: name = data.name, url, token } = service;
-    const axiosConfig: AxiosRequestConfig = {};
-    if (!url) {
-      request.log.error(`'url' not set for Home Assistant service ${name}. Data not sent.`);
-      return;
-    }
-
+    request.log.info(`Sending data to homeassistant at ${url} for device ${deviceLabel} for state ${stateName} `);
     try {
-      request.log.info(`Sending data to homeassistant at ${url} for device ${name} `);
-      const payload = {
-        state: temperature,
-        attributes: {
-          unit_of_measurement: '°F',
-          friendly_name: `${name} Temp`,
-        },
-      };
       const { status, data: resData } = await axios.post(
-        `${url}/api/states/sensor.${name}_temp`,
+        `${url}/api/states/sensor.${deviceLabel}_${stateName}`,
         payload,
         {
+          ...axiosConfig,
           headers: { Authorization: `Bearer ${token}` },
         },
       );
-
       // eslint-disable-next-line no-console
       request.log.info(resData, `${status} response from ${url}`);
     } catch (err) {
       request.log.error(err.response.data, `Error from homeassistant at ${url} for device ${name}`);
     }
+  };
+
+  services.forEach(async (service) => {
+    const { deviceLabel = data.name, url, token } = service;
+    const axiosConfig: AxiosRequestConfig = {};
+    if (!url) {
+      request.log.error(`'url' not set for homeassistant service ${deviceLabel}. Data not sent.`);
+    }
+
+    const {
+      temperature,
+      battery,
+      gravity,
+      angle,
+    } = data;
+
+    postData(url, deviceLabel, 'temperature', temperature, '°F', token, axiosConfig);
+    postData(url, deviceLabel, 'battery', battery, 'V', token, axiosConfig);
+    postData(url, deviceLabel, 'gravity', gravity, 'SG', token, axiosConfig);
+    postData(url, deviceLabel, 'angle', angle, '°', token, axiosConfig);
   });
 };

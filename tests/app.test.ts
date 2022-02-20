54e8ba64b213cfda92f306d8c50af826e24bf474
Chris Scott <chris@iamzed.com>
2022-02-20 16:44:09 -0500

    Add test.

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,6 @@
 import { test } from 'tap';
 import build from '../src/app';
+import getConfig from '../src/config';
 
 test('requests the "/" route', async (t) => {
   const app = build({
@@ -14,4 +15,8 @@ test('requests the "/" route', async (t) => {
   });
 
   t.equal(response.statusCode, 200, 'returns a status code of 200');
+});
+
+test('example config is valid', async (t) => {
+  t.doesNotThrow(() => getConfig(`${__dirname}/../config.example.json`), 'does not throw an exception');
 });

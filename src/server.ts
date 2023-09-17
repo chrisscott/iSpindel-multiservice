d235f09d1b286e02cbe64ae8813ef8675043bf06
Chris Scott <chris@iamzed.com>
2023-09-17 09:43:36 -0400

    Listen on 0.0.0.0 to work with hosting services

--- src/server.ts
+++ src/server.ts
@@ -48,7 +48,7 @@ export default async (): Promise<void> => {
   server.get(serverPath, async () => 'Data should be POSTed to this endpoint');
   server.post(serverPath, opts, async () => 'ok');
 
-  server.listen({ port }, (err) => {
+  server.listen({ port, host: '0.0.0.0' }, (err) => {
     if (err) {
       server.log.error(err);
       process.exit(1);
